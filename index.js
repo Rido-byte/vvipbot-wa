99dd98e058b33286433f117525e18e889e8f848c
Rido-byte <83914000+Rido-byte@users.noreply.github.com>
2024-06-13 17:59:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
+const BotName = 'NRXd BOT'; 
+const instagram = '_nottyourmine'; 
+const aktif = 'Kamis 13 juni 2024 jam 18.00 Wib ';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Rido\n' // Nama kamu
+            + 'ORG:NRXd;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=62882003701412:+62-882-003-701-412\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
